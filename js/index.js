3a9439d31b82d4330845ffddde974a3dd6a7e4c1
Philip Holtzman <phil.holtzman@gmail.com>
2022-06-23 15:28:52 +0300

    up to milestone 4

--- js/index.js
+++ js/index.js
@@ -1,25 +1,71 @@
-const userInput = document.getElementById("givenInput");
-const result = document.getElementById("result");
+// const USER_INPUT = document.getElementById("givenInput"); // input
+// const RESULT = document.getElementById("result"); // result display
+// const FIB_URL = "http://localhost:5050/fibonacci/{$number}"; // local server
 
-const submitButton = (document.getElementById("submitButton").onclick =
-  calculateSeq);
+const fibForm = document.getElementById('fibForm');
 
-    function calculateSeq() {
-  console.log("..calculating sequence.. ");
+fibForm.addEventListener('submit', function(e){
+  e.preventDefault();
+  fib();
+})
 
-  let fIndex = userInput.value;
+// let num = USER_INPUT.value;
 
-  // a + b are the numbers added, c is the index
-  let a = 0,
-    b = 1,
-    c = fIndex;
 
-  for (let i = 2; i <= fIndex; i++) {
-    c = a + b;
-    a = b;
-    b = c;
+
+function fib()  {
+  document.getElementById("res").innerHTML = ''
+  number = document.getElementById("givenInput").value;
+  server = `http://localhost:5050/fibonacci/${number}`;
+  fetch(server).then((response) => {
+    response.json().then( data => { document.getElementById("res").innerHTML = data.result});
+  console.log(response)});
   }
 
-  console.log(c);
-  result.innerHTML = c;
-}
+// function calculateSeqServer() {
+//   //let fIndex = USER_INPUT.value;
+
+//   let server = `http://localhost:5050/fibonacci/11`
+//   fetch(server)
+//   .then((response) => {
+//     response.json().then(data => {
+//       document.getElementById("res") = data.res
+//     });
+//   })
+// }
+
+
+
+
+
+// OLD FUNCTION (LOCAL)
+// function calculateSeqLocal() {
+  
+//   console.log("..calculating sequence.. ");
+
+//   let fIndex = USER_INPUT.value;
+
+//   console.log("checking input for errors");
+//   if (fIndex > 50) {
+//     console.log("ERROR too big");
+//     // make pop up type red error crap
+//   } else if (fIndex == 42) {
+//     console.log("ERROR Secret of life");
+//     RESULT.innerText = "ERROR Secret of life";
+//     ////// change text and append new html error
+//   } else {
+//     // a + b are the numbers added, c is the index
+//     let a = 0,
+//       b = 1,
+//       c = fIndex;
+
+//     for (let i = 2; i <= fIndex; i++) {
+//       c = a + b;
+//       a = b;
+//       b = c;
+//     }
+
+//     console.log(c);
+//     RESULT.innerText = c;
+//   }
+// }
